c8e85291d48da67fdabfdfcd62abc579ba270781
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-05-03 19:09:16 +0700

    finally done release 1

--- composition.js
+++ composition.js
@@ -152,37 +152,9 @@ function parseIngredient(fileName) {
   // }
 }
 
-function parseIngredientAmount(fileName) {
-  let fs = require("fs");
-  let buf = fs.readFileSync("bahan.txt", 'utf-8');
-  let optionsArr = buf.split("\n");
-
-  nameCookieArr = [];
-  ingredientArr = [];
-  for (let i = 0; i < optionsArr.length - 1; i++) {
-    nameCookieArr.push(optionsArr[i].split("=")[0]);
-    ingredientArr.push(optionsArr[i].split("=")[1].trim())
-  }
-
-  for (let i = 0; i < ingredientArr.length; i++) {
-    ingredientArr[i] = ingredientArr[i].split(",")
-  }
-
-
-  let amount = [];
-  let ingredientName = [];
-  for (let i = 0; i < ingredientArr.length; i++) {
-    for (let j = 0; j < ingredientArr[i].length; j++) {
-      amount.push(ingredientArr[i][j].split(":")[0].trim());
-      ingredientName.push(ingredientArr[i][j].split(":")[1]);
-    }
-  }
-}
-
 let file = "bahan.txt";
 
 let batchCookies = CookieFactory.create(file)
-
 
 let sugarFreeFoods = CookieFactory.cookieRecommendation("tuesday", batchCookies);
 console.log("sugar free cakes are :\n");
